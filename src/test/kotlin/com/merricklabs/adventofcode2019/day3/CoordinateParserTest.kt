94e30b7809c44f3a1566579565ea350691346779
David Merrick <david@david-merrick.com>
2019-12-17 19:13:30 -0800

    Got working solution

--- src/test/kotlin/com/merricklabs/adventofcode2019/day3/CoordinateParserTest.kt
+++ src/test/kotlin/com/merricklabs/adventofcode2019/day3/CoordinateParserTest.kt
@@ -2,6 +2,7 @@ package com.merricklabs.adventofcode2019.day3
 
 import io.kotlintest.shouldBe
 import org.testng.annotations.Test
+import kotlin.math.abs
 
 class CoordinateParserTest {
 
@@ -11,7 +12,7 @@ class CoordinateParserTest {
         val lineB = Line("U7,R6,D4,L4")
 
         val intersection = lineA.pointSet.intersect(lineB.pointSet)
-        val closest = intersection.map { Math.abs(it.first) + Math.abs(it.second) }.min()!!
+        val closest = intersection.map { abs(it.first) + abs(it.second) }.min()!!
 
         closest shouldBe 6
     }
@@ -22,7 +23,7 @@ class CoordinateParserTest {
         val lineB = Line("U62,R66,U55,R34,D71,R55,D58,R83")
 
         val intersection = lineA.pointSet.intersect(lineB.pointSet)
-        val closest = intersection.map { Math.abs(it.first) + Math.abs(it.second) }.min()!!
+        val closest = intersection.map { abs(it.first) + abs(it.second) }.min()!!
 
         closest shouldBe 159
     }
@@ -33,9 +34,24 @@ class CoordinateParserTest {
         val lineB = Line("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
 
         val intersection = lineA.pointSet.intersect(lineB.pointSet)
-        val closest = intersection.map { Math.abs(it.first) + Math.abs(it.second) }.min()!!
+        val closest = intersection.map { abs(it.first) + abs(it.second) }.min()!!
 
         closest shouldBe 135
+    }
+
+    @Test
+    fun `Challenge input`() {
+        val input = this::class.java.getResourceAsStream("input.txt")
+                .bufferedReader()
+                .readLines()
+
+        val lineA = Line(input[0])
+        val lineB = Line(input[1])
+
+        val intersection = lineA.pointSet.intersect(lineB.pointSet)
+        val closest = intersection.map { abs(it.first) + abs(it.second) }.min()!!
+
+        closest shouldBe 896
     }
 
 
